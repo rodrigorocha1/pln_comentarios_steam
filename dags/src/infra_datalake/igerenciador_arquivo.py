9a1227321b18dfa9da9a83b5e18886868e95699b
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-08-31 19:35:49 -0300

    Método para guadar aquivo

--- dags/src/infra_datalake/igerenciador_arquivo.py
+++ dags/src/infra_datalake/igerenciador_arquivo.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Dict, Optional
 
 
 class IGerenciadorArquivo(ABC):
+
+    @abstractmethod
+    def abrir_arquivo(self, caminho_arquivo: str, ) -> Optional[str]:
+        pass
 
     @abstractmethod
     def guardar_arquivo(self, dado: Dict, caminho_arquivo: str):

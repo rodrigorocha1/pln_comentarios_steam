8b563e340041d32c811163fb1e48170778172401
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-09-06 18:43:48 -0300

    Correção

--- dags/ddags_task.py
+++ dags/ddags_task.py
@@ -1,5 +1,4 @@
 from airflow import DAG
-
 from airflow.operators.empty import EmptyOperator
 from airflow.providers.http.operators.http import HttpOperator
 from airflow.operators.python import PythonOperator
@@ -8,14 +7,23 @@ from datetime import datetime
 
 from src.processo_etl.processo_etl import ProcessoEtl
 
-data = datetime.now().date().strftime('%Y_%m_%d')
-
+# IDs de jogos
 lista_ids = [244850, 275850, 359320, 392160, 1623730, 255710, 949230]
+
+
+def run_bronze(id_jogo, data):
+    ProcessoEtl(caminho=None).executar_processo_etl_bronze(id_jogo=id_jogo, data=data)
+
+def run_prata(id_jogo, data):
+    ProcessoEtl(caminho=None).executar_processo_etl_prata_comentarios_tratados(id_jogo=id_jogo, data=data)
+
+def run_prata_cb(id_jogo, data):
+    ProcessoEtl(caminho=None).executar_processo_etl_prata_comentarios_brutos(id_jogo=id_jogo, data=data)
+
 
 with DAG(
         dag_id="dag_comentarios_steam",
         start_date=datetime(2025, 1, 1),
-
         catchup=False,
 ) as dag:
     inicio_dag = EmptyOperator(task_id="inicio_dag")
@@ -27,43 +35,35 @@ with DAG(
         endpoint='minio/health/ready',
         response_check=lambda response: response.status_code == 200,
         log_response=True,
-
     )
 
     dag_erro = EmptyOperator(task_id='dag_erro', trigger_rule='one_failed')
 
-    with TaskGroup('task_steam_reviews_bronze', dag=dag) as tg_steam_bronze:
-        lista_tasks = []
+    with TaskGroup('task_steam_reviews_bronze') as tg_steam_bronze:
         for i in lista_ids:
-            salvar_camada_bronze = PythonOperator(
+            PythonOperator(
                 task_id=f"executar_processo_etl_bronze_{i}",
-                python_callable=ProcessoEtl(caminho=None).executar_processo_etl_bronze,
-                op_kwargs={"id_jogo": i, "data": data}
+                python_callable=run_bronze,
+                op_kwargs={"id_jogo": i, "data": "{{ ds_nodash }}"}
             )
-            lista_tasks.append(lista_ids)
 
-    with TaskGroup('task_steam_reviews_prata', dag=dag) as tg_steam_prata:
-        lista_tasks = []
+    with TaskGroup('task_steam_reviews_prata') as tg_steam_prata:
         for i in lista_ids:
-            salvar_camada_prata = PythonOperator(
+            PythonOperator(
                 task_id=f"executar_processo_etl_prata_{i}",
-                python_callable=ProcessoEtl(caminho=None).executar_processo_etl_prata_comentarios_tratados,
-                op_kwargs={"id_jogo": i, "data": data}
-
+                python_callable=run_prata,
+                op_kwargs={"id_jogo": i, "data": "{{ ds_nodash }}"}
             )
-            lista_tasks.append(salvar_camada_prata)
 
-    with TaskGroup('task_steam_reviews_prata_comentario_tratado', dag=dag) as tg_steam_prata_comentarios_bruto:
-        lista_tasks = []
+    with TaskGroup('task_steam_reviews_prata_comentario_tratado') as tg_steam_prata_comentarios_bruto:
         for i in lista_ids:
-            salvar_camada_prata_comentarios_brutos = PythonOperator(
+            PythonOperator(
                 task_id=f"executar_processo_etl_prata_cb_{i}",
-                python_callable=ProcessoEtl(caminho=None).executar_processo_etl_prata_comentarios_brutos,
-                op_kwargs={"id_jogo": i, "data": data}
+                python_callable=run_prata_cb,
+                op_kwargs={"id_jogo": i, "data": "{{ ds_nodash }}"}
             )
 
     fim_dag = EmptyOperator(task_id="fim_dag", trigger_rule='all_done')
 
     inicio_dag >> checar_url_minio >> tg_steam_bronze >> tg_steam_prata >> tg_steam_prata_comentarios_bruto >> fim_dag
-
     checar_url_minio >> dag_erro >> fim_dag
--- um.py
+++ um.py
@@ -1,14 +1,17 @@
 import duckdb
 from langdetect import detect, DetectorFactory
+from spacy import displacy
 import spacy
 import unicodedata
 import re
+import pathlib
+
 #  read_json_auto
 AWS_ACCESS_KEY_ID = "meuusuario"
 AWS_SECRET_ACCESS_KEY = "minhasenha123"
 AWS_ENDPOINT_URL = "172.40.0.21:9000"  # host:porta
 BUCKET_NAME = "meu-bucket"
-ARQUIVO_PATH = "datalake/prata/data_2025_09_03/jogo_244850/reviews.parquet"
+ARQUIVO_PATH = "datalake/prata/comentarios_brutos/jogo_244850/reviews_bruto.parquet"
 con = duckdb.connect('dados_reviews/reviews.duckdb')
 con.execute(f"SET s3_access_key_id='{AWS_ACCESS_KEY_ID}';")
 con.execute(f"SET s3_secret_access_key='{AWS_SECRET_ACCESS_KEY}';")
@@ -21,5 +24,32 @@ df = con.execute(f"""
     SELECT * FROM read_parquet('s3://{BUCKET_NAME}/{ARQUIVO_PATH}')
 """).df()
 
-# Configura seed do langdetect (para resultados consistentes)
-print(df.value_counts())
+lista_setenca = df["valores"].dropna().astype(str).tolist()
+
+# Carrega modelo SpaCy
+nlp = spacy.load("pt_core_news_sm")
+
+output_dir = pathlib.Path("saida_displacy")
+output_dir.mkdir(exist_ok=True)
+
+for i, texto in enumerate(lista_setenca, start=1):
+    doc = nlp(texto)
+    print(f"\n🔹 Review {i}: {texto}")
+
+    # Exibir dependência sintática dos tokens
+    for token in doc:
+        print(
+            f"token={token.text:<15} "
+            f"dep={token.dep_:<10} "
+            f"head={token.head.text: <15}  "
+            f"pos={token.pos_: <20} "
+            f"tag={token.tag_: <20}"
+            f"lemma={token.lemma_: <20}"
+            f"morph={str(token.morph):<40}"
+        )
+
+        for ent in doc.ents:
+            print('doc ents' * 20)
+            print(ent.text, ent.label_, ent.conjuncts)
+
+    print("=" * 100)
